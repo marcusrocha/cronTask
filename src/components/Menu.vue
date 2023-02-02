e6b94e6e69770385af0d0a0b0476cbc28c0f5d3e
Your Name <you@example.com>
2023-02-02 23:42:32 +0000

    Improve the menu size

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -5,13 +5,13 @@ import { BIconHouse } from 'bootstrap-icons-vue';
 </script>
 
 <template>
-  <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark vh-100" style="width: 280px;">
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark vh-100" style="width: 180px;">
     <a href="/" class="align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi me-2" width="40" height="32">
         <use xlink:href="#bootstrap"></use>
       </svg>
       <span class="fs-4">
-        <img alt="Minha logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+        <img alt="Minha logo" class="logo" src="@/assets/logo.png" width="70" height="70" />
       </span>
      
     </a>
